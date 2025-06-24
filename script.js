570a2b52d01f41dc957f4595b5a9bda76650765e
Sree-Lakshmi-Dileep <sreelakshmidileep2004@gmail.com>
2025-06-24 14:56:51 +0530

    highlight task based on deadline

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function sortTodos() {
   const sortValue = document.getElementById("sort").value;
   const tbody = document.querySelector("#todos tbody");
   const rows = Array.from(tbody.querySelectorAll("tr"));
-
+   
   if (sortValue === "sort-all") {
     window.location.reload(); 
     return;
   }
 
-  const rowData = rows.map(row => {
+    const rowData = rows.map(row => {
     const cells = row.querySelectorAll("td");
     return {
       title: cells[0].innerText.toLowerCase(),
@@ -23,8 +23,8 @@ function sortTodos() {
  
   if (sortValue === "deadline") {
     rowData.sort((a, b) => {
-      const [d1, m1, y1] = a.deadline.split("/").map(Number);
-      const [d2, m2, y2] = b.deadline.split("/").map(Number);
+      const [d1, m1, y1] = a.deadline.split("-").map(Number);
+      const [d2, m2, y2] = b.deadline.split("-").map(Number);
       const dateA = new Date(y1, m1 - 1, d1);
       const dateB = new Date(y2, m2 - 1, d2);
       return dateA - dateB;
@@ -39,6 +39,7 @@ function sortTodos() {
   tbody.innerHTML = "";
   rowData.forEach(item => tbody.appendChild(item.element));
 }
+  
 
 
 //filter function
@@ -83,6 +84,18 @@ function deleteTodo(icon){
 
 }
 
+//function to highlight task based on deadline
+
+
+function getDaysLeft(dateStr){
+    const [y,m,d] = dateStr.split("-").map(Number);
+  const today =new Date();
+  const deadline= new Date(y,m-1,d);
+  today.setHours(0,0,0,0);
+  deadline.setHours(0,0,0,0)
+  const diff = deadline-today;
+   return Math.ceil(diff/(1000*60*60*24));
+}
 
 //finish functiion
 
@@ -103,26 +116,42 @@ function deleteTodo(icon){
 };
 
 function renderTodos(){
+  const tbody = document.querySelector("#todos tbody");
   tbody.innerHTML="";
   const incomplete = todos.filter(todo => ! todo.completed);
   const completed = todos.filter(todo =>  todo.completed);
   const all = [...incomplete,...completed];
-   
+  
   all.forEach(todo=>{
-    const style = todo.completed?'text-decoration:line-through;':'';
+
+  //function to highlight row based on deadline
+    const daysLeft = getDaysLeft(todo.deadline);
+    let colorStyle ="";
+    if(daysLeft < 0) {
+      colorStyle = "background-color:red;color:white;";
+    }
+    else if(daysLeft >= 0 && daysLeft <= 2){
+      colorStyle= "background-color:orange;color:white;";
+    }
+    else if(daysLeft >= 3 && daysLeft <= 5){
+      colorStyle= "background-color:yellow;color:white;";
+    }
+
+    const strikeStyle = todo.completed?'text-decoration:line-through;':'';
+    const combinedstyle =`${strikeStyle} ${colorStyle}`.trim();
 
     const row = document.createElement("tr");
     row.innerHTML =
-    ` <td style="${style}">${todo.title}</td>
-      <td style="${style}">${todo.deadline}</td>
-      <td style="${style}">${todo.category}</td>
-      <td >
+    ` <td style="${combinedstyle}">${todo.title}</td>
+      <td style="${combinedstyle}">${todo.deadline}</td>
+      <td style="${combinedstyle}">${todo.category}</td>
+      <td " ${combinedstyle}">
         <i class="fa-solid fa-check" style="color: #0ff560;" onclick="finishTodo(this)"></i>
         <i class="fa-solid fa-pen" style="color: #74C0FC; onclick="editTodo(this)"></i>
         <i class="fa-solid fa-trash" style="color: #7a6e43;" onclick="deleteTodo(this)"></i>
       </td>`;
-      tbody.appendChild(row);
-      updateStatus();
+    tbody.appendChild(row);
+    updateStatus();
   });
 }
 
@@ -130,15 +159,14 @@ function renderTodos(){
 //function -> status update
 
 function updateStatus(){
-  const total =todos.length;
-  const completed =todos.filter(t=>t.completed).length;
+  const total = todos.length;
+  const completed =todos.filter(todo=>todo.completed).length;
   const pending=total-completed;
 
   document.getElementById("total-count").textContent=total;
   document.getElementById("completed-count").textContent=completed;
   document.getElementById("pending-count").textContent=pending;
 }
-
 
 renderTodos();
 
